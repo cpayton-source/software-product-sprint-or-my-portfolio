849dc11d0e3cabbdc51a33dc70fbd7d0c829fcde
Cameron <cpaytoncodes@gmail.com>
2021-03-06 02:39:32 +0000

    Add redirect

--- portfolio/src/main/java/com/google/sps/servlets/ContactFormServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/ContactFormServlet.java
@@ -20,5 +20,6 @@ public class ContactFormServlet extends HttpServlet {
 
     // Write the value to the response so the user can see it.
     response.getWriter().println("You submitted: " + textValue);
+    response.sendRedirect("https://cpayton-sps-spring21.appspot.com");
   }
 }
